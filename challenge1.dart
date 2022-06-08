2d39d7689cf6ef6d7774f0a80d8fc3c3e0fa9089
ZhraaBaharAluloom <alwarda@hotmail.co.uk>
2022-06-08 12:01:58 -0700

    zahraa-review

--- challenge1.dart
+++ challenge1.dart
@@ -1,12 +1,12 @@
 /**
  * Task 1:
- * Create a function named `printName`
- * - that just prints your name on the screen
+ * Create a function called `printName`
+ * - that prints your name on the screen.
  */
 
 /**
  * Task 2:
- * Create a function named `printAge`
+ * Create a function called `printAge`
  * - that takes a birth year as a parameter,
  * - and prints the age on the screen.
  * - Age = current year - birth
@@ -14,9 +14,9 @@
 
 /**
  * Task 3:
- * Create a function named `printHello`
- * - that takes 2 parameters, name, and language
- * - language can be passed in different values, here are the accepted values:-
+ * Create a function called `printHello`
+ * - that takes 2 parameters, name, and language.
+ * - Language can be passed in different values, here are the accepted values:-
  * -- en: it should print `Hello NAME`
  * -- es: it should print `Hola NAME`
  * -- fr: it should print `Bonjour NAME`
@@ -25,7 +25,7 @@
 
 /**
  * Task 4:
- * Create a function named `printMax`
- * - that takes 2 parameters as numbers
- * - should print out the bigger number
+ * Create a function called `printMax`
+ * - that takes two numbers as parameters,
+ * - and prints out the bigger number
  */
